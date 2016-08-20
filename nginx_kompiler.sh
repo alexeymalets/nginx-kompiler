0d2bcb621cfdfd088ca487ba11c59993ff3a8fd4
Alexey Malets <alex@expil.pro>
2016-08-20 21:20:17 +0300

    Public test version 0.4
    Added the ability to install an unlimited number of modules
    Fixed all known bugs

--- nginx_kompiler.sh
+++ nginx_kompiler.sh
@@ -14,7 +14,7 @@ loc_yes='Yes'
 loc_no='No'
 loc_up_ug_system='Run the system update?'
 loc_choose='Choose'
-loc_install_module='Installing modules Nginx nchan, nginx-upload-progress-module, echo-nginx-module и nginx-rtmp-module?'
+loc_install_module='Installing modules Nginx nchan, nginx-upload-progress-module, echo-nginx-module and nginx-rtmp-module?'
 loc_det_v_nginx='Determine the configuration of Nginx'
 loc_mkdir_mod='Create a directory under modules'
 loc_go_to_dir='Go to directory'
@@ -43,6 +43,10 @@ loc_link_mod_ex='Example:'
 loc_cin_dir_name_mod='Enter the name of the directory with the module'
 loc_add_nginx_mod_cho='Add an additional module Nginx?'
 loc_no_text='You have not entered anything, so an additional module  was not added.'
+loc_al_module='You have already added this module before'
+loc_install_module_yo='Install other Nginx modules?'
+loc_pre_upg='Prepare the system to run the script'
+loc_link='Link'
 #Spechil sim
 
 #Detect OS
@@ -81,19 +85,32 @@ echo "${loc_your_os} ${dist} ${osv}"
 
 #Check install nginx
 checknginxstatus=0
-
-module_install_nchan(){
-	echo "${loc_dow_mod} Nchan"
-	wget https://github.com/slact/nchan/archive/master.zip
-	echo "${loc_unpack}"
+shtelm=0
+module_install_stnd(){
+	echo "${loc_pre_upg}"
+	if ["$(ls /etc/nginx/ | grep kompiler)" ]; then
+		rm -rf /etc/nginx/kompiler
+	fi
 	if [ -z "$(dpkg -l | grep unzip)" ]; then
 		apt-get install -y unzip
 	fi
+	
+	echo "${loc_mkdir_dir} nginxtmp"
+	mkdir nginxtmp 
+	
+	echo "${loc_mkdir_mod} /etc/nginx/kompiler"
+	mkdir /etc/nginx/kompiler
+	
+	echo "${loc_go_to_dir} /etc/nginx/kompiler"
+	cd /etc/nginx/kompiler
+	
+	echo "${loc_dow_mod} Nchan"
+	wget https://github.com/slact/nchan/archive/master.zip
+	echo "${loc_unpack}"
 	unzip master.zip
 	echo "${loc_delete_zip} master.zip"
 	rm -rf master.zip
-}
-module_install_nuprogressmodule(){
+
 	echo "${loc_dow_mod} nginx-upload-progress-module"
 	wget https://github.com/masterzen/nginx-upload-progress-module/archive/master.zip
 	echo "${loc_unpack}"
@@ -103,25 +120,21 @@ module_install_nuprogressmodule(){
 	unzip master.zip
 	echo "${loc_delete_zip} master.zip"
 	rm -rf master.zip
-	
-}
-module_install_echomodule(){
+
 	echo "${loc_dow_mod} echo-nginx-module"
 	wget https://github.com/openresty/echo-nginx-module/archive/master.zip
 	echo "${loc_unpack}"
 	unzip master.zip
 	echo "${loc_delete_zip} master.zip"
 	rm -rf master.zip
-}
-module_install_rtmp(){
+
 	echo "${loc_dow_mod} nginx-rtmp-module"
 	wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
 	echo "${loc_unpack}"
 	unzip master.zip
 	echo "${loc_delete_zip} master.zip"
 	rm -rf master.zip
-}
-module_install_l(){
+	
 	echo "${loc_add_nginx_mod_cho}"
 	echo "1) ${loc_yes}"
 	echo "2) ${loc_no}"
@@ -143,6 +156,74 @@ module_install_l(){
 			cin_dir_name_mod_nginx_l="--add-module=/etc/nginx/kompiler/${cin_dir_name_mod}"
 		fi
 	fi
+	confnginxredactor="--add-module=/etc/nginx/kompiler/echo-nginx-module-master --add-module=/etc/nginx/kompiler/nchan-master --add-module=/etc/nginx/kompiler/nginx-rtmp-module-master --add-module=/etc/nginx/kompiler/nginx-upload-progress-module-master ${cin_dir_name_mod_nginx_l}"
+}
+module_install_l(){
+	echo "${loc_pre_upg}"
+	if ["$(ls /etc/nginx/ | grep kompiler)" ]; then
+		rm -rf /etc/nginx/kompiler
+	fi
+	if [ -z "$(dpkg -l | grep unzip)" ]; then
+		apt-get install -y unzip
+	fi
+	modules_nginx_install_ch=1
+	shtelm=0
+	
+	echo "${loc_mkdir_dir} nginxtmp"
+	mkdir nginxtmp 
+	
+	echo "${loc_mkdir_mod} /etc/nginx/kompiler"
+	mkdir /etc/nginx/kompiler
+	
+	echo "${loc_go_to_dir} /etc/nginx/kompiler"
+	cd /etc/nginx/kompiler
+	
+	while [ "$modules_nginx_install_ch" -eq 1 ]; do
+		shtelm=$(($shtelm + 1))
+		echo "${loc_add_nginx_mod_cho}"
+		echo "1) ${loc_yes}"
+		echo "2) ${loc_no}"
+		read -p "${loc_choose}: " add_nginx_mod_cho
+		if [ $add_nginx_mod_cho -eq 1 ]; then
+			echo "${loc_link_mod_nginx}"
+			echo "${loc_link_mod_ex}:https://github.com/arut/nginx-rtmp-module/archive/master.zip"
+			read -p "${loc_link}: " module_nginx_ob
+			if [ -z "$module_nginx_ob" ]; then
+				echo "${loc_no_text}"
+				continue
+			else
+				echo "${loc_mkdir_mod} /etc/nginx/kompiler/module_${shtelm}"
+				mkdir module_$shtelm
+				echo "${loc_go_to_dir} /etc/nginx/kompiler/module_${shtelm}"
+				cd module_$shtelm
+				echo "${loc_dow_mod} $module_nginx_ob"
+				wget $module_nginx_ob
+				echo "${loc_unpack}"
+				unzip master.zip
+				echo "${loc_delete_zip} *.zip"
+				rm -rf master.zip
+				cin_dir_name_mod=$(ls)
+				if [ -z "$cin_dir_name_mod" ]; then
+					echo "${loc_no_text}"
+					rm -rf /etc/nginx/kompiler/module_$shtelm
+					continue
+				else
+					cd && cd nginxtmp
+					if [ -z "$(grep -rn "/etc/nginx/kompiler/${cin_dir_name_mod}" confnginx.ini)" ]; then
+						echo "--add-module=/etc/nginx/kompiler/module_$shtelm/${cin_dir_name_mod} " | tee -a confnginx.ini
+					else
+						echo "${loc_al_module}"
+					fi
+				fi
+			fi
+		else
+			modules_nginx_install_ch=0
+		fi
+	done
+	cd && cd nginxtmp
+	touch confnginxredactor.ini
+	tr "\n" " " < confnginx.ini | tee -a confnginxredactor.ini
+	confnginxredactor=$(cat confnginxredactor.ini)
 }
 nginx_modules_install(){
 		echo "${loc_up_ug_system}"
@@ -152,29 +233,23 @@ nginx_modules_install(){
 		if [ $upgrade_packet_system -eq 1 ]; then
 			apt-get update && apt-get -y upgrade
 		fi
-		echo "${loc_install_module}"
-		echo "1) ${loc_yes}"
-		echo "2) ${loc_no}"
+		echo "1) ${loc_install_module}"
+		echo "2) ${loc_install_module_yo}"
 		read -p "${loc_choose}: " install_module_nginx
-		if [ $install_module_nginx -eq 1 ]; then
-			echo "${loc_mkdir_mod} /etc/nginx/kompiler"
-			mkdir /etc/nginx/kompiler
-			
-			echo "${loc_go_to_dir} /etc/nginx/kompiler"
-			cd /etc/nginx/kompiler
-			
-			module_install_nchan
-			module_install_nuprogressmodule
-			module_install_echomodule
-			module_install_rtmp
-		fi
 		
-		module_install_l
-		echo "${loc_go_to_dir}"
+		case $install_module_nginx in
+			1)
+				module_install_stnd
+			;;
+			2)
+				module_install_l
+			;;
+			*)
+				exit 0
+			;;
+		esac
+
 		cd
-		
-		echo "${loc_mkdir_dir}"
-		mkdir nginxtmp 
 		
 		echo "${loc_go_to_dir} nginxtmp"
 		cd nginxtmp
@@ -193,12 +268,13 @@ nginx_modules_install(){
 		echo "${loc_install_pak}"
 		apt-get -y install build-essential libpcre3 libpcre3-dev openssl libssl-dev libxml2-dev libxslt-dev libgd-dev libgeoip-dev libperl-dev
 		
-		echo "${loc_install_pak}"
-		./configure  --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_perl_module=dynamic --with-threads --with-stream --with-stream_ssl_module --with-stream_geoip_module=dynamic --with-http_slice_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6 --with-http_v2_module --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' --with-ld-opt='-Wl,-z,relro -Wl,--as-needed' --add-module=/etc/nginx/kompiler/echo-nginx-module-master --add-module=/etc/nginx/kompiler/nchan-master --add-module=/etc/nginx/kompiler/nginx-rtmp-module-master --add-module=/etc/nginx/kompiler/nginx-upload-progress-module-master $cin_dir_name_mod_nginx_l
+		echo "${loc_conf_pak}"
+		./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-http_ssl_module --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module --with-http_secure_link_module --with-http_stub_status_module --with-http_auth_request_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_perl_module=dynamic --with-threads --with-stream --with-stream_ssl_module --with-http_slice_module --with-mail --with-mail_ssl_module --with-file-aio --with-ipv6 --with-http_v2_module --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2' --with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,--as-needed' $confnginxredactor
 		
-		echo "${loc_install_pak}"
+		echo "${loc_conf_install_pak}"
 		make && make install
-		
+		cd
+		rm -rf nginxtmp
 		checknginxstatus=1
 }
 if [ "${dist}" = "debian" ] || [ "${dist}" = "ubuntu" ]; then
@@ -249,7 +325,7 @@ if [ $checknginxstatus -eq 1 ]; then
 			echo "${loc_upgrade_nginx_sf}!"		
 		else
 			echo "${loc_upgrade_nginx_faild}"
-			echo "GitHub https://github.com/alexeymalets/nginx-install-auto/issues"
+			echo "GitHub https://github.com/alexeymalets/nginx-kompiler"
 			echo "${loc_srv_admin} http://svradmin.ru/"
 			echo "${loc_myhosti}(24/7) https://myhosti.pro"
 			echo "${loc_error} :$(systemctl status nginx.service)"
@@ -261,7 +337,7 @@ if [ $checknginxstatus -eq 1 ]; then
 			echo "${loc_upgrade_nginx_sf}!"		
 		else
 			echo "${loc_upgrade_nginx_faild}"
-			echo "GitHub https://github.com/alexeymalets/nginx-install-auto/issues"
+			echo "GitHub https://github.com/alexeymalets/nginx-kompiler"
 			echo "${loc_srv_admin} http://svradmin.ru/"
 			echo "${loc_myhosti}(24/7) https://myhosti.pro"
 			echo "${loc_error} :$(tail -n 10 /var/log/nginx/error.log)"
